9687decadfe122de1f6e34681fe69d66b9787be0
RinaOcean <antyfifa@gmail.com>
2021-01-19 19:00:44 +0200

    task-2 done

---
+++ js/task-2.js
@@ -0,0 +1,35 @@
+// Задание 2
+// В HTML есть пустой список ul#ingredients.
+// В JS есть массив строк.
+// Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li,
+//  после чего вставит все li за одну операцию в список ul.ingredients.
+//  Для создания DOM-узлов используй document.createElement().
+
+const ingredients = [
+  "Картошка",
+  "Грибы",
+  "Чеснок",
+  "Помидоры",
+  "Зелень",
+  "Приправы",
+];
+
+// const ingrRef = document.querySelector("#ingredients");
+// const listRef = document.createElement("li");
+// listRef.classList.add("product");
+// // listing.textContent = ingredient;
+// console.log(ingrRef);
+// ingrRef.appendChild(listRef);
+
+const createList = (ingredient) => {
+  const ingrRef = document.querySelector("#ingredients");
+
+  const listRef = document.createElement("li");
+  listRef.classList.add("product");
+  listRef.textContent = ingredient;
+  ingrRef.appendChild(listRef);
+};
+
+const ingredientsListing = ingredients.forEach((ingredient) =>
+  createList(ingredient)
+);
