4b3c90854ee4ec18bbfd47b8ff5390c8eb1c2270
RinaOcean <antyfifa@gmail.com>
2021-01-25 00:32:55 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -7,11 +7,15 @@
 
 const inputRef = document.querySelector("#validation-input");
 
-inputRef.addEventListener("input", checkValidation);
+// inputRef.addEventListener("input", checkValidation);
 inputRef.addEventListener("blur", checkValidation);
 
-function checkValidation(event) {
-  event.target.value.length < inputRef.dataset.length
-    ? inputRef.classList.add("invalid")
-    : inputRef.classList.replace("invalid", "valid");
+function checkValidation() {
+  if (inputRef.value.length === Number(inputRef.dataset.length)) {
+    inputRef.classList.add("valid");
+    inputRef.classList.remove("invalid");
+  } else {
+    inputRef.classList.add("invalid");
+    inputRef.classList.remove("valid");
+  }
 }
